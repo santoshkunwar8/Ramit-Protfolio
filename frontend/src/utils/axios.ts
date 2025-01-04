e641f0d3f5aa76c58d5a975ae385ad94f8e449c3
Ramit Neupane <149955018+RamitNpn@users.noreply.github.com>
2025-01-04 13:14:25 +0000

    changed frontend axios code

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const AxiosInstance = axios.create({
-    baseURL: `http://localhost:8000/api`,
+    baseURL: `https://backend.ramitneupane.com.np/api`,
     withCredentials:true
 })
 
